ac5069e48df489fa2c43bc94d657dab03f0b882f
BluLupo <hersel1991@gmail.com>
2024-03-02 13:39:19 +0100

    add retention period

--- db_bash_backup-main/config.sh
+++ db_bash_backup-main/config.sh
@@ -9,3 +9,6 @@ backup_dir="db_backup"
 
 # List of database names to back up (add any database name separated by a space)
 databases=("db1" "db2" "db3")
+
+# Number of days to retain backup files
+backup_retention_days=7
