1c099eefc98f94ac75050a390d73edbc1e6f3e50
BluLupo <hersel1991@gmail.com>
2024-03-02 13:38:37 +0100

    add retention period

--- db_bash_backup-main/db.sh
+++ db_bash_backup-main/db.sh
@@ -5,16 +5,19 @@ source config.sh
 
 # Cycle through all the databases and back up each one
 for db_name in "${databases[@]}"; do
-    # Compongo il nome del file di backup
+    # Compose the backup file name
     backup_file="$backup_dir/$db_name-$(date +%Y%m%d%H%M%S).sql"
 
-    # I run the mysqldump command to export the database
+    # Execute the mysqldump command to export the database
     mysqldump --user="$db_user" --password="$db_pass" --host="$db_host" --port="$db_port" --databases "$db_name" > "$backup_file"
 
-    # If the backup was successful, I see a confirmation message
+    # Check if the backup was successful
     if [ $? -eq 0 ]; then
-        echo "Backup di $db_name completato con successo in $backup_file"
+        echo "Backup of $db_name completed successfully in $backup_file"
     else
-        echo "Si è verificato un errore durante il backup di $db_name"
+        echo "An error occurred during the backup of $db_name"
     fi
+
+    # Delete backups older than specified retention period
+    find "$backup_dir" -type f -name "$db_name-*.sql" -mtime +$backup_retention_days -exec rm {} \;
 done
